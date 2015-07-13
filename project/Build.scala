0da8ad643b1dcc14851ab3ea7308e5a664b93f48
Natal Vande Casteele <natal@libius.be>
2015-07-13 14:58:43 +0200

    Version increment

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object GeolatteNoSqlBuild extends Build {
 
   val appName = "geolatte-nosql"
-  val appVersion = "1.3-SNAPSHOT"
+  val appVersion = "1.4-SNAPSHOT"
 
   //Resolvers
   lazy val commonResolvers = Seq(
